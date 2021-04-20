14fa4e60a211cc7b02103f60f47b3c3b63f98012
alexandraPatica <alexandrapatica@yahoo.com>
2021-04-20 15:00:19 +0300

    2nd

---
+++ proj/zybo-z7/zybo-z7.gen/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
@@ -0,0 +1,31 @@
+--Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2020.2 (win64) Build 3064766 Wed Nov 18 09:12:45 MST 2020
+--Date        : Tue Apr 13 19:03:42 2021
+--Host        : DESKTOP-P54K02F running 64-bit major release  (build 9200)
+--Command     : generate_target design_1_wrapper.bd
+--Design      : design_1_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity design_1_wrapper is
+  port (
+    leds_4bits_tri_o : out STD_LOGIC_VECTOR ( 3 downto 0 )
+  );
+end design_1_wrapper;
+
+architecture STRUCTURE of design_1_wrapper is
+  component design_1 is
+  port (
+    leds_4bits_tri_o : out STD_LOGIC_VECTOR ( 3 downto 0 )
+  );
+  end component design_1;
+begin
+design_1_i: component design_1
+     port map (
+      leds_4bits_tri_o(3 downto 0) => leds_4bits_tri_o(3 downto 0)
+    );
+end STRUCTURE;
